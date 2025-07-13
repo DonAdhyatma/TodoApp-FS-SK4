63fc70ee272265c839b3ce13e73e0caebf980ef7
Danni Adhyatma <dannirachman2@gmail.com>
2025-07-13 18:28:14 +0700

    fix: correct code for file root page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -15,12 +15,12 @@ export default function Home() {
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
+          <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
               src/app/page.js
             </code>
             .
-          </li>
-          <li className="tracking-[-.01em]">
+        </li>
+        <li className="tracking-[-.01em]">
             Save and see your changes instantly.
           </li>
         </ol>
@@ -41,21 +41,21 @@ export default function Home() {
             />
             Deploy now
           </a>
-          <a
+        <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+                    target="_blank"
+          rel="noopener noreferrer"
+        >
+          Read our docs
+        </a>
+</div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
+                    target="_blank"
           rel="noopener noreferrer"
         >
           <Image
@@ -70,7 +70,7 @@ export default function Home() {
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
+                    target="_blank"
           rel="noopener noreferrer"
         >
           <Image
@@ -85,7 +85,7 @@ export default function Home() {
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
+                    target="_blank"
           rel="noopener noreferrer"
         >
           <Image
@@ -98,6 +98,7 @@ export default function Home() {
           Go to nextjs.org →
         </a>
       </footer>
-    </div>
+      </div>
+    </main>
   );
 }
